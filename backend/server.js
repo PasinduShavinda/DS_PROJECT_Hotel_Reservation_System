d4d46e43ab69b44f41f71631ab1363f011149d2a
PasinduShavinda <pasindu.shv@gmail.com>
2022-05-08 21:06:28 +0530

    server configurations done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ const taxiRouts = require('./routes/shv_taxi_route');
 
 // Middlewares
 app.use(bodyParser.json());
+app.use(cors());
 app.use(taxiRouts);
 
 const PORT = 8001;
