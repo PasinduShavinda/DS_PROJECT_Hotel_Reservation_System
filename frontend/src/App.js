3dce041c606279268ba4c5eb4b92bae8a06f3b22
Sugandhi <sugandhi.kalansooriya@gmail.com>
2022-05-14 00:16:32 +0530

    Room Reservation function completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,24 @@ import Home from './components/Home';
 import Shv_Taxi_Booking_Home from './components/shv_TaxiReservation/shv_Taxi_Booking_Home';
 import Footer from './components/shv_Footer/Footer';
 
+
+
+
+
+
+
+
+import RoomsHome from "./components/sug_RoomReservations/sug_RoomsHome";
+import HomePage from "./components/sug_HomePage/sug_HomePage";
+
+import AllroomReservations from "./components/sug_RoomReservations/sug_AllroomReservations";
+import BookedDetails from "./components/sug_RoomReservations/sug_BookedDetails";
+import EditReservation from "./components/sug_RoomReservations/sug_EditReservations";
+import ReservationDetails from "./components/sug_RoomReservations/sug_reservationdetails";
+
+//import ReserveRoom from "./components/sug_RoomReservations/sug_ReserveRoom";
+import RoomBook from "./components/sug_RoomReservations/sug_roombook";
+
 export default class App extends Component {
     render(){
 
@@ -20,7 +38,7 @@ export default class App extends Component {
           <div>
           <BrowserRouter>
 
-          <Route path = "/"  exact component = {Home}/>
+          
           <Route path = "/TaxiHome"  exact component = {Shv_Taxi_Reservations_Home_Page}/>
           <Route path = "/AllTaxi" component = {Shv_All_Taxi_Reservations}/>
           <Route path = "/BookTaxi" component = {Shv_Create_Taxi_Reservations}/>
@@ -31,6 +49,15 @@ export default class App extends Component {
           <Route path = "/location" component = {LocationService}/>
           <Route path = "/AdminTaxiReport" component = {Shv_AdminTaxiReport}/>
           <Route path = "/TaxiBookHome" component = {Shv_Taxi_Booking_Home}/>
+
+
+          <Route path="/Allreservations" exact component={AllroomReservations}></Route>
+          <Route path="/reserveroom" exact component={RoomBook}></Route>
+          <Route path="/editReservation/:id" exact component={EditReservation}></Route>
+          <Route path="/reservationdetails/:id" exact component={ReservationDetails}></Route>
+         <Route path="/bookeddetails" exact component={BookedDetails}></Route>
+         <Route path="/roomhomepage" exact component={RoomsHome}></Route>
+          <Route path="/" exact component={HomePage}></Route>
 
           </BrowserRouter>
           <br/><br/>
